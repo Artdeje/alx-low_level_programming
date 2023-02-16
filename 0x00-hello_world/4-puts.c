b53ae58b02da792334647a9e6c6b0ad1de67f824
Artdeje <artdeje@gmail.com>
2023-02-16 14:49:42 +0200

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: A C program that prints with put function
- *     *
- *      * Return: Always 0 (Success)
- *      */
+ *main - Entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programing is a tricky game like candy crush");
+	return (0);
 }
