3cfc99cd887b6d73366cae47318860f6ba1e973a
Artdeje <artdeje@gmail.com>
2023-04-05 06:51:09 -0700

    Commit 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,35 +2,35 @@
 #include <stdlib.h>
 
 /*
- * C Program to Display all the Nodes in a Linked List without using
+ * C Program to print all the Nodes in a Linked List without using
  * Recursion
  */
- 
+
 struct node
 {
     int a;
     struct node *next;
 };
- 
+
 void generate(struct node **);
 void display(struct node*);
 void delete(struct node **);
- 
+
 int main()
 {
     struct node *head = NULL;
- 
+
     generate(&head);
     display(head);
     delete(&head);
     return 0;
 }
- 
+
 void generate(struct node **head)
 {
     int num = 10, i;
     struct node *temp;
- 
+
     for (i = 0; i < num; i++)
     {
         temp = (struct node *)malloc(sizeof(struct node));
@@ -47,7 +47,7 @@ void generate(struct node **head)
         }
     }
 }
- 
+
 void display(struct node *head)
 {
     while (head != NULL)
@@ -57,7 +57,7 @@ void display(struct node *head)
     }
     printf("\n");
 }
- 
+
 void delete(struct node **head)
 {
     struct node *temp;
