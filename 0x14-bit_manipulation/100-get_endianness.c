0eb99d0a99bbf36c6e7ea32af3e801f470ee43dc
Artdeje <artdeje@gmail.com>
2023-04-12 08:47:43 -0700

    Fnl commits

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+/*
+ *Owner: Artdeje
+ *Tittle: Bit manipulation
+ */
+#include <stdio.h>
+/**
+ * Get endianness &  Checks ones.
+ * Returns: If endian - 0.
+ *         If little endian - 1.
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+	if (*endian == 1)
+	return (1);
+	return (0);
+}
