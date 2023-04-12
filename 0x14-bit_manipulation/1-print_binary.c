a4b3e25ca5919fc78d8b7782487f0699201071bd
Artdeje <artdeje@gmail.com>
2023-04-12 09:20:57 -0700

    Commit 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,38 @@
-/**
- *Code: Arthur Dejemeppe
- *Codetype: C program
- * Use: Coding educational project for ALX cohort12.
- * */
-#include <stdio.h>
 #include <stdlib.h>
-/**
- * C program which is running a number to binary
- * Amazing p is variable is presented
- * */
-char *toBinary(int p, int len)																																			{																																										char* binary =	(char*)malloc(sizeof(char) * len);
-	int k = 0;
-	for (unsigned i =	(1 << len - 1); i > 0; i = i / 2) {
-	binary[k++] =	(p & i) ? '1' : '0';
-	}
-	binary[k] = '\0';
-	return binary;
-}
-	int main(void)
-{
-	int p = 5;
-	int len = 11;
-	char* binary = toBinary(p, len);
-	printf("The binary representation of %d is %s", p, binary);
-	free(binary);
+#include <stdio.h>
+#include "main.h"
 
-	return 0;
+/**
+  * print_binary - Prints the binary representation of the number
+  * @b: The number to representing in binary in program
+  * Showing the return
+  * Return: 0
+  */
+void print_binary(unsigned long int b)
+{
+	if (b == 0)
+	{
+	_putchar('0');
+	return;
+}
+	_divide(b);
+}
+
+/**
+  * Divided in most codes
+  * @b: shows all alerts
+  *
+  * Return: still the same
+  */
+void _divide(unsigned long int b)
+{
+	if (b < 1)
+		return;
+
+	_divide(b >> 1);
+
+	if (b & 1)
+		_putchar('1');
+	else
+		_putchar('0');
 }
--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -13,6 +13,8 @@ int get_endianness(void)
 	int num = 1;
 	char *endian = (char *)&num;
 	if (*endian == 1)
+
 	return (1);
 	return (0);
+
 }
