08b9a752c17d49d7243a099d7d881e4a34532c7f
Artdeje <artdeje@gmail.com>
2023-04-12 08:02:06 -0700

    Commit 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,34 +1,29 @@
 /**
-*Project owner: Artdeje rutura
-*Code reference: https://techiedelight.com
-**/
-
+ *Code: Arthur Dejemeppe
+ *Codetype: C program
+ * Use: Coding educational project for ALX cohort12.
+ * */
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
- */
-char* toBinary(int p, int len)
-{
-    char* binary = (char*)malloc(sizeof(char) * len);
-    int k = 0;
-    for (unsigned i = (1 << len - 1); i > 0; i = i / 2) {
-        binary[k++] = (p & i) ? '1' : '0';
-    }
-    binary[k] = '0';
-    return binary;
+ * C program which is running a number to binary
+ * Amazing p is variable is presented
+ * */
+char *toBinary(int p, int len)																																			{																																										char* binary =	(char*)malloc(sizeof(char) * len);
+	int k = 0;
+	for (unsigned i =	(1 << len - 1); i > 0; i = i / 2) {
+	binary[k++] =	(p & i) ? '1' : '0';
+	}
+	binary[k] = '\0';
+	return binary;
 }
-
-int main(void)
+	int main(void)
 {
-    int p = 5;
-    int len = 30;
+	int p = 5;
+	int len = 11;
+	char* binary = toBinary(p, len);
+	printf("The binary representation of %d is %s", p, binary);
+	free(binary);
 
-    char* binary = toBinary(p, len);
-    printf("The binary presentation of %d is %s", p, binary);
-    free(binary);
-
-    return 0;
+	return 0;
 }
