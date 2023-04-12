8550060f6d4476a667d1192ddd4396d9c49f3b5b
Artdeje <artdeje@gmail.com>
2023-04-12 05:05:10 -0700

    Test commit 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- *binary to unit takes binary and makes in an integer
- *A: is a letter and is siblings to b
- *B: will be a letter siblings to a
- *Return: Artdejeeee
+ *binary_to_uint - takes binary and makes in an integer lived along
+ *a: is a letter and is siblings to b
+ *b: is also sibling to a
+ *Return: peepeepoopoo
  */
 
-unsigned int binary_to_uint(const char *b);
-int main(void);
 
+unsigned int binary_to_uint(const char *a);
 {
-	    {int index;
+	int index;
 	unsigned int dec_value = 0;
 
 	if (b == NULL)
 		return (0);
-	    }
 
-	for (index = 0; b[index] != '\0'; index++)
-	{
+	for (index = 0; b[index] != '\0'; index++);
+
 		if (b[index] == '1')
 			dec_value = ((2 * dec_value) + 1);
 		else if (b[index] == '0')
