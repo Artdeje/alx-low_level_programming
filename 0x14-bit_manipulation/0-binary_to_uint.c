afb71d324acdf899c53d889a041094fe5534030b
Artdeje <artdeje@gmail.com>
2023-04-14 08:33:26 -0700

    New codes 00

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,26 @@
-#include <stdio.h>
+/**
+*Project by: Arthur N.D
+*ALX Cohort12 2023
+**/
 #include "main.h"
 
 /**
- *binary_to_uint - takes binary and makes in an integer lived along
- *a: is a letter and is siblings to b
- *b: is also sibling to a
- *Return: number have been count
- */
-
-
-unsigned int binary_to_uint(const char *a);
+*binary_to_unit -converts a binary number to unsigned int
+*@b: string containing the binary numbers
+*
+*Return: The converted numbers
+**/
+unsigned int binary_to_unit(const char *b);
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+    int i;
+    unsigned int dec_val =0;
+    if(!b)
+        return(0);
+    for (i=0; b[i]; i++)
+    {
+        if(b[i] < '0' || b[i] > '1') return (0);
+        dec_val = 2* dec_val + (b[i] - '0');
+    }
 
-	if (a == '\0')
-		return (0);
-
-	for (len = 0; a[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
-	{
-		if (a[len] != '0' && a[len] != '1')
-			return (0);
-
-		num += (a[len] - '0') * mult;
-		mult *= 2;
-	}
-	return (num);
+    return (dec_val);
 }
