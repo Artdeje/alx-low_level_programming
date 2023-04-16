00d7bfbbe9120ba74d5681a10205d254fc84f767
Artdeje <artdeje@gmail.com>
2023-04-16 11:38:24 -0700

    Commit 11

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include <stdiio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,7 +7,7 @@
  * @text_content: content writed in the file.
  * Return: It must return 1 if it success.returns otherwise -1 if it fails.
 **/
-int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content);
 {
 	int o, w, len = 0;
 
