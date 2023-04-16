4bd1bb1ba348910f3e243cd4f4e5ad9eea76d4e1
Artdeje <artdeje@gmail.com>
2023-04-16 11:32:15 -0700

    New stuff for hobbie project

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <stdiio.h>
+#include "main.h"
+
+/**
+ * create_file - creates the I/O  file in C program.
+ * @filename: filename will be generated as we command the instructions.
+ * @text_content: content writed in the file.
+ * Return: It must return 1 if it success.returns otherwise -1 if it fails.
+**/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
