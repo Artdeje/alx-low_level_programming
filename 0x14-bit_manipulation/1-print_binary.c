59bcf926236ca6903e71bf984b6f4ad5b83cf731
Artdeje <artdeje@gmail.com>
2023-04-16 04:41:46 -0700

    Ntacyo it like fan

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,38 +1,14 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
-  * print_binary - Prints the binary representation of the number
-  * @b: The number to representing in binary in program
-  * Showing the return
-  * Return: 0
-  */
-void print_binary(unsigned long int b)
+ * print_binary - Print showing the binary representation of a number.
+ * @n: The number in binary presentation.
+ */
+void print_binary(unsigned long int n)
 {
-	if (b == 0)
-	{
-	_putchar('0');
-	return;
-}
-	_divide(b);
-}
+	if (n > 1)
+		print_binary(n >> 1);
 
-/**
-  * Divided in most codes
-  * @b: shows all alerts
-  *
-  * Return: still the same
-  */
-void _divide(unsigned long int b)
-{
-	if (b < 1)
-		return;
-
-	_divide(b >> 1);
-
-	if (b & 1)
-		_putchar('1');
-	else
-		_putchar('0');
+	_putchar((n & 1) + '0');
 }
